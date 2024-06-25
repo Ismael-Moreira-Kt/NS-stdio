6fb8bc5fffa59b87827f9cafec40b33f2de00bd4
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-06-25 19:27:25 +0100

    fix: Update to the stdin function.

--- src/stdio.asm
+++ src/stdio.asm
@@ -1,17 +1,15 @@
-section .bss
-    buffer resb 256
-
-
-
 section .text
     global _stdin
 
 
-    _stdin:
-        mov rax, 0
-        mov rdi, 0
-        mov rsi, buffer
-        mov rdx, 255
-        syscall
 
-        ret
+_stdin:
+    push rbp
+    mov rbp, rsp
+
+    mov rax, 0
+    mov rdi, 0
+    syscall
+    
+    leave
+    ret
